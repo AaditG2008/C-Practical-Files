c71c38ad4163547a6178258c6269e956c8ba65d0
Aadit <gupta.aadit0555@gmail.com>
2026-08-29 20:19:37 +0530

    Fix formatting

--- ASCII_~1.C
+++ ASCII_~1.C
@@ -10,5 +10,3 @@ printf("The ASCII value of '%c' is %d\n",a,a);
 printf("Name-Aadit Gupta\nRoll Number-56");
 getch();
 }
-//include the following lines if you run code in Turbo C++ 
-//include<conio.h> and use clrscr() and getch() functions in the code to clear the screen and hold the terminal in Turbo C++.
