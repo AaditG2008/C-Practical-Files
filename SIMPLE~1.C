a200e1fcc8cbfbaaf2bd4f850fbf646e2e6bad6a
Aadit <gupta.aadit0555@gmail.com>
2026-08-29 20:19:01 +0530

    Fix formatting issues in SIMPLE~1.C

--- SIMPLE~1.C
+++ SIMPLE~1.C
@@ -17,5 +17,3 @@ printf("The quotient is %d\n",f);
 printf("Name-Aadit Gupta\nRoll Number-56");
 getch();
 }
-//include the following lines if you run code in Turbo C++ 
-//include<conio.h> and use clrscr() and getch() functions in the code to clear the screen and hold the terminal in Turbo C++.
