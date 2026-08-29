d0881b153a8e2aa84a19f6fcf8b48e58f6ae0e55
Aadit <gupta.aadit0555@gmail.com>
2026-08-29 20:12:48 +0530

    Add files via upload

---
+++ ASCII_~1.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+clrscr();
+printf("Enter the character:\n");
+scanf("%c",&a);
+printf("The ASCII value of '%c' is %d\n",a,a);
+printf("Name-Aadit Gupta\nRoll Number-56");
+getch();
+}
+//include the following lines if you run code in Turbo C++ 
+//include<conio.h> and use clrscr() and getch() functions in the code to clear the screen and hold the terminal in Turbo C++.
---
+++ SIMPLE~1.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d,e,f;
+clrscr();
+printf("Enter the numbers:\n");
+scanf("%d %d",&a,&b);
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+printf("The sum is %d\n",c);
+printf("The difference is %d\n",d);
+printf("The product is %d\n",e);
+printf("The quotient is %d\n",f);
+printf("Name-Aadit Gupta\nRoll Number-56");
+getch();
+}
+//include the following lines if you run code in Turbo C++ 
+//include<conio.h> and use clrscr() and getch() functions in the code to clear the screen and hold the terminal in Turbo C++.
