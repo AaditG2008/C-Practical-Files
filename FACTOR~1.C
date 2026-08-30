87c02477e266b7eb050314b7d41c7d8b1c098a2e
Aadit <gupta.aadit0555@gmail.com>
2026-08-30 12:55:31 +0530

    Factorial

---
+++ FACTOR~1.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i;
+    unsigned long long int f=1;
+    clrscr();
+    printf("Enter the integer:\n");
+    scanf("%d",&n);
+    if(n<0) {
+	printf("Factorial for a negative number does not exist\n");
+    } else {
+	for(i=1;i<=n;i++) {
+	    f=f*i;
+	}
+    printf("Factorial of %d is %llu\n",n,f);
+    }
+    printf("Name - Aadit Gupta\nRoll Number - 56");
+    getch();
+}
